30208cae5285a1a22f3dff26df1fa5e574d9db7e
Bin Shao <ben@fiture.com>
2020-05-23 15:08:45 +0800

    feat(ClingDemo): add Cling Demo for test SSDP
    - DatagramIOImpl
    - MulticastReceiverImpl

---
+++ ClingDemo/app/src/main/java/com/mobodev/spikes/clingdemo/browser/BrowserUpnpService.java
@@ -0,0 +1,37 @@
+package com.mobodev.spikes.clingdemo.browser;
+
+import org.fourthline.cling.UpnpServiceConfiguration;
+import org.fourthline.cling.android.AndroidUpnpServiceConfiguration;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+import org.fourthline.cling.model.types.ServiceType;
+import org.fourthline.cling.model.types.UDAServiceType;
+
+/**
+ * @author Christian Bauer
+ */
+// DOC:CLASS
+public class BrowserUpnpService extends AndroidUpnpServiceImpl {
+
+    @Override
+    protected UpnpServiceConfiguration createConfiguration() {
+        return new AndroidUpnpServiceConfiguration() {
+
+            // DOC:REGISTRY
+            @Override
+            public int getRegistryMaintenanceIntervalMillis() {
+                return 7000;
+            }
+            // DOC:REGISTRY
+
+            // DOC:SERVICE_TYPE
+            @Override
+            public ServiceType[] getExclusiveServiceTypes() {
+                return new ServiceType[]{
+                        new UDAServiceType("SwitchPower")
+                };
+            }
+            // DOC:SERVICE_TYPE
+        };
+    }
+}
+// DOC:CLASS
