43c85f7426fcae824f9b534af5987d2ee1929b04
Bin Shao <ben@fiture.com>
2020-05-23 18:03:11 +0800

    feat(SSDP): add more code

--- SSDP/app/src/main/java/com/mobodev/spikes/ssdp/DatagramIO.java
+++ SSDP/app/src/main/java/com/mobodev/spikes/ssdp/DatagramIO.java
@@ -1,17 +1,77 @@
 package com.mobodev.spikes.ssdp;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.SocketException;
 
 public class DatagramIO implements Runnable {
+    private static final String TAG = "DatagramIO";
 
-    protected MulticastSocket socket; // For sending unicast & multicast, and reveiving unicast
+    private int timeToLive = 4;
+    private int maxDatagramBytes = 640;
+
+    private MulticastSocket socket; // For sending unicast & multicast, and reveiving unicast
+    private InetAddress localAddress;
 
     public DatagramIO() {
+        try {
+            socket = new MulticastSocket(0);
+            socket.setTimeToLive(timeToLive);
+            socket.setReceiveBufferSize(262144); // Keep a backlog of incoming datagrams if we are not fast enough
 
+            localAddress = socket.getLocalAddress();
+            Log.i(TAG, "Creating bound socket (for datagram input/output) on: " + localAddress);
+        } catch (IOException e) {
+            Log.e(TAG, "error create DatagramIO", e);
+        }
+    }
+
+    synchronized public void stop() {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+        }
     }
 
     @Override
     public void run() {
+        Log.v(TAG, "Entering blocking receiving loop, listening for UDP datagrams on: " + localAddress);
 
+        while (true) {
+
+            try {
+                byte[] buf = new byte[maxDatagramBytes];
+                DatagramPacket datagram = new DatagramPacket(buf, buf.length);
+
+                socket.receive(datagram);
+
+                Log.v(TAG,
+                        "UDP datagram received from: "
+                                + datagram.getAddress().getHostAddress()
+                                + ":" + datagram.getPort()
+                                + " on: " + localAddress
+                );
+
+
+                router.received(datagramProcessor.read(localAddress.getAddress(), datagram));
+
+            } catch (SocketException ex) {
+                Log.w(TAG, "Socket closed");
+                break;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        try {
+            if (!socket.isClosed()) {
+                Log.i(TAG, "Closing unicast socket");
+                socket.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
--- SSDP/app/src/main/java/com/mobodev/spikes/ssdp/MulticastReceiver.java
+++ SSDP/app/src/main/java/com/mobodev/spikes/ssdp/MulticastReceiver.java
@@ -1,8 +1,97 @@
 package com.mobodev.spikes.ssdp;
 
+import android.util.Log;
+
+import java.net.DatagramPacket;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+
 public class MulticastReceiver implements Runnable {
+    private static final String TAG = "MulticastReceiver";
+    private int maxDatagramBytes = 640;
+    public static final int UPNP_MULTICAST_PORT = 1900;
+
+    public static final String IPV4_UPNP_MULTICAST_GROUP = "239.255.255.250";
+
+    protected InetSocketAddress multicastAddress;
+    protected MulticastSocket socket;
+
+    public MulticastReceiver() {
+        try {
+
+            Log.i(TAG, "Creating wildcard socket (for receiving multicast datagrams) on port: " + UPNP_MULTICAST_PORT);
+            multicastAddress = new InetSocketAddress(IPV4_UPNP_MULTICAST_GROUP, UPNP_MULTICAST_PORT);
+
+            socket = new MulticastSocket(UPNP_MULTICAST_PORT);
+            socket.setReuseAddress(true);
+            socket.setReceiveBufferSize(32768); // Keep a backlog of incoming datagrams if we are not fast enough
+
+            Log.i(TAG, "Joining multicast group: " + multicastAddress);
+            socket.joinGroup(multicastAddress.getAddress());
+
+        } catch (Exception ex) {
+            Log.e(TAG, "error create MulticastReceiver", ex);
+        }
+    }
+
+    synchronized public void stop() {
+        if (socket != null && !socket.isClosed()) {
+            try {
+                Log.i(TAG, "Leaving multicast group");
+                socket.leaveGroup(multicastAddress.getAddress());
+                // Well this doesn't work and I have no idea why I get "java.net.SocketException: Can't assign requested address"
+            } catch (Exception ex) {
+                Log.w(TAG, "Could not leave multicast group: " + ex);
+            }
+            // So... just close it and ignore the log messages
+            socket.close();
+        }
+    }
+
     @Override
     public void run() {
+        Log.i(TAG, "Entering blocking receiving loop, listening for UDP datagrams on: " + socket.getLocalAddress());
+        while (true) {
 
+            try {
+                byte[] buf = new byte[maxDatagramBytes];
+                DatagramPacket datagram = new DatagramPacket(buf, buf.length);
+
+                socket.receive(datagram);
+
+                InetAddress receivedOnLocalAddress =
+                        networkAddressFactory.getLocalAddress(
+                                multicastInterface,
+                                multicastAddress.getAddress() instanceof Inet6Address,
+                                datagram.getAddress()
+                        );
+
+                log.fine(
+                        "UDP datagram received from: " + datagram.getAddress().getHostAddress()
+                                + ":" + datagram.getPort()
+                                + " on local interface: " + multicastInterface.getDisplayName()
+                                + " and address: " + receivedOnLocalAddress.getHostAddress()
+                );
+
+                router.received(datagramProcessor.read(receivedOnLocalAddress, datagram));
+
+            } catch (SocketException ex) {
+                Log.w(TAG, "Socket closed");
+                break;
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        try {
+            if (!socket.isClosed()) {
+                Log.i(TAG, "Closing multicast socket");
+                socket.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
